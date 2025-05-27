96309bfd081aae5259946236a9b6c364e2b08628
Bakamashine <surajaaminu09@gmail.com>
2025-05-27 23:49:58 +0300

    update main.py

--- main.py
+++ main.py
@@ -4,16 +4,30 @@ from vk.methods import get_credentials, get_user_credentials, get_message, send_
 from vk.types import Message, EventMessage
 import asyncio
 import requests
+from random import choice
+
+PRODUCTION = "http://dadamapt.beget.tech/post.php"
+DEV = "http://localhost:9001/post.php"
 
 def PostResponse(url, data):
     response = requests.post(url, data)
     return response.status_code
 
+def random_text():
+    words = [
+        "Редактирую техническое задание",
+        "Разрабатываю веб-сайт",
+        "Знакомлюсь с технологиями Борхиммаш",
+        "",
+    ]
+
+    return f"Филиппович Иван Александрович. Приступил к практике. {choice(words)}"
+
 
 async def main(server, key, ts, vk_chat_ids, access_token, cookie, pts):
-    print(f"Всего chat_id: {len(vk_chat_ids)}")
+    # print(f"Всего chat_id: {len(vk_chat_ids)}")
     print("Отправка сообщений...")
-    text ="Должно прийти в 21.52"
+    text = random_text()
     for chat_id in vk_chat_ids:
         try:
             send_message(access_token, chat_id, text)
@@ -21,6 +35,6 @@ async def main(server, key, ts, vk_chat_ids, access_token, cookie, pts):
         except Exception as e:
             logging.exception(f"Ошибка при отправке сообщения в чат {chat_id}: {e}")
     print("Сообщения отправлены!")
-    result = PostResponse("http://localhost:9001/post.php", {"name": text})
+    result = PostResponse(PRODUCTION, {"name": text})
     print("Результат отправки POST запроса: ", result)
     exit(0)
